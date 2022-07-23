90f4b24e43616807c35a3aefb36202160d0c613e
Chethan C K <chethanckm66@gmail.com>
2022-07-23 22:43:13 +0530

    Simple Tic-Tac-Toe - Welcome to the battlefield!

--- src/com/chethan/Main.java
+++ src/com/chethan/Main.java
@@ -3,6 +3,12 @@ package com.chethan;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[][] grid = {{"X ", "O ", "X "}, {"O ", "X ", "O "}, {"X ", "X ", "O "}};
+        for(String[] str: grid){
+            for(String st: str){
+                System.out.print(st);
+            }
+            System.out.println();
+        }
     }
 }
